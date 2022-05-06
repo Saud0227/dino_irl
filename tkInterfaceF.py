c244265b500a4813a1c3629534a51175c0f04e6a
Saud0227 <Saud02Tech@gmail.com>
2022-05-06 15:43:23 +0200

    rect interface done, not working atm

--- tkInterfaceF.py
+++ tkInterfaceF.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from time import sleep
+
 from serviceH import configHandler
 
 from vectors import vector
@@ -39,5 +42,51 @@ class tkInterface:
         return self._alive
     
     
-    def rect(x, y, xS, yS):
+    def _rectPolyDraw(self, posL):
+        id = self.root.create_polygon(posL[0],posL[1],posL[2],posL[3],posL[4],posL[5],posL[6],posL[7], fill='red')
+        return(id)
+    
+    
+    
+    def rect(self, *args):
+        ccr = ["iiii", "vii", "vv"]
+        code = ""
+        for i in args:
+            if type(i) == int:
+                code+="i"
+            elif type(i) == vector:
+                code+="v"
+            else:
+                raise ValueError("Non suported input (use int or vector)")
+            
+        try:
+            inType = ccr.index(code)
+        except ValueError:
+            return False
         
+        
+        if inType == 0:
+            
+            corn = [args[0],args[1],args[0]+args[2],args[1],args[0]+args[2],args[1]+args[3],args[0],args[1]+args[3]]
+            print(corn)
+            id = self._rectPolyDraw(corn)
+            
+            return id
+           
+        
+        
+        
+        
+
+
+if __name__ == '__main__':
+    a = tkInterface(tk)
+    while a.alive():
+        # a.preLoop()
+        id = a.rect(500, 500,100,100)
+        # canvas
+        sleep(5)
+        print("!")
+        a.update()
+    
+    
